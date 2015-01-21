285ee55add99e3d5a655f3abb8292ded7d4ac4fd
Arthur Himmel <himmel.arthur@gmail.com>
2015-01-21 15:52:28 +0100

    Added User Entity

---
+++ src/Resor/Bundle/CoreBundle/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Acme\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
