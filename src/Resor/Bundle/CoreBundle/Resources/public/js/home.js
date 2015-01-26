a5cdd13e1e8a640576fc557db387da6e9f0d9076
Arthur Himmel <himmel.arthur@gmail.com>
2015-01-26 15:52:58 +0100

    Datetimepicker in home page

--- src/Resor/Bundle/CoreBundle/Resources/public/js/home.js
+++ src/Resor/Bundle/CoreBundle/Resources/public/js/home.js
@@ -1,8 +1,21 @@
 (function ($, top) {
 
     $(function () {
-        var placeInput = $('.js-place-input');
+        var placeInput = $('.js-place-input'),
+            fromInput = $('.js-from-input'),
+            toInput = $('.js-to-input');
+
         placeInput.geocomplete();
+
+        $.each([fromInput, toInput], function (i, input) {
+            input.datetimepicker({
+                lang: 'fr',
+                timepicker: false,
+                minDate: 0,
+                format: 'd/m/Y'
+            });
+        });
+
     });
 
 }(jQuery, window));
