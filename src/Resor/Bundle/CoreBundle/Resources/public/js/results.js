37c462312d88cd9c78eb795216b5d199b21aebf3
Arthur Himmel <himmel.arthur@gmail.com>
2015-01-28 13:42:24 +0100

    #3 - Display list with Angular

--- src/Resor/Bundle/CoreBundle/Resources/public/js/results.js
+++ src/Resor/Bundle/CoreBundle/Resources/public/js/results.js
@@ -1,4 +1,4 @@
-(function ($, top, GMaps) {
+(function ($, top, GMaps, angular) {
 
     $(function () {
         var changePosition = $('.js-change-position'),
@@ -15,4 +15,45 @@
 
     });
 
-}(jQuery, window, GMaps));
+}(jQuery, window, GMaps, angular));
+
+(function () {
+    var app = angular.module("resultsApp", []);
+
+    var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in dignissim ex. Ut efficitur libero sed ipsum laoreet, a laoreet nunc consequat. Vestibulum mollis quis dui non commodo. Donec nec neque id nulla volutpat maximus. Nulla facilisi. Nullam ultricies lacinia diam, nec consectetur massa congue ac. Phasellus scelerisque at enim sed rhoncus. Ut ut ex leo. Nunc eu libero leo. Phasellus placerat luctus interdum. Duis efficitur laoreet dolor, malesuada rutrum neque congue sit amet. Aliquam semper arcu sapien. Suspendisse diam neque, ullamcorper sed pulvinar nec, tincidunt non mauris. Nulla at sagittis risus. Nullam aliquam, lectus in fermentum blandit, eros risus pellentesque purus, et interdum dolor magna eu arcu.";
+
+    app.controller('ResultCtrl', function ($scope) {
+        $scope.results = [
+            {
+                title: "Camping des flots bleus",
+                description: lorem,
+                price: "34"
+
+            },
+            {
+                title: "Camping de l'océan",
+                description: lorem,
+                price: "27"
+
+            },
+            {
+                title: "Camping de la plage",
+                description: lorem,
+                price: "39"
+
+            },
+            {
+                title: "Camping du soleil",
+                description: lorem,
+                price: "42"
+
+            },
+            {
+                title: "Camping Serge",
+                description: lorem,
+                price: "19"
+
+            }
+        ];
+    });
+}());
