c9ed6b00fb21198e466c66e07b2be2b7b5357bcb
Arthur Himmel <himmel.arthur@gmail.com>
2015-01-28 16:27:48 +0100

    #3 - Filtered list

--- src/Resor/Bundle/CoreBundle/Resources/public/js/results.js
+++ src/Resor/Bundle/CoreBundle/Resources/public/js/results.js
@@ -20,6 +20,12 @@
 (function () {
     var app = angular.module("resultsApp", []);
 
+    app.filter("capitalize", function () {
+        return function (s) {
+            return s[0].toUpperCase() + s.slice(1);
+        };
+    });
+
     var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in dignissim ex. Ut efficitur libero sed ipsum laoreet, a laoreet nunc consequat. Vestibulum mollis quis dui non commodo. Donec nec neque id nulla volutpat maximus. Nulla facilisi. Nullam ultricies lacinia diam, nec consectetur massa congue ac. Phasellus scelerisque at enim sed rhoncus. Ut ut ex leo. Nunc eu libero leo. Phasellus placerat luctus interdum. Duis efficitur laoreet dolor, malesuada rutrum neque congue sit amet. Aliquam semper arcu sapien. Suspendisse diam neque, ullamcorper sed pulvinar nec, tincidunt non mauris. Nulla at sagittis risus. Nullam aliquam, lectus in fermentum blandit, eros risus pellentesque purus, et interdum dolor magna eu arcu.";
 
     app.controller('ResultCtrl', function ($scope) {
@@ -27,33 +33,68 @@
             {
                 title: "Camping des flots bleus",
                 description: lorem,
-                price: "34"
+                price: "34",
+                features: ["pool", "animals", "spa", "jacuzzi"]
 
             },
             {
                 title: "Camping de l'océan",
                 description: lorem,
-                price: "27"
+                price: "27",
+                features: ["spa", "jacuzzi"]
 
             },
             {
                 title: "Camping de la plage",
                 description: lorem,
-                price: "39"
+                price: "39",
+                features: ["pool", "spa", "jacuzzi"]
 
             },
             {
                 title: "Camping du soleil",
                 description: lorem,
-                price: "42"
+                price: "42",
+                features: ["pool", "animals"]
 
             },
             {
                 title: "Camping Serge",
                 description: lorem,
-                price: "19"
+                price: "19",
+                features: ["spa", "jacuzzi"]
 
             }
         ];
+
+        $scope.filters = [
+            {
+                name: "pool",
+                on: true
+            },
+            {
+                name: "spa",
+                on: false
+            },
+            {
+                name: "jacuzzi",
+                on: false
+            },
+            {
+                name: "animals",
+                on: false
+            },
+            {
+                name: "children",
+                on: false
+            }
+        ];
+
+        $scope.filterByFeatures = function (result) {
+            return $scope.filters.reduce(function(memo, filter){
+                return memo && !(filter.on && result.features.indexOf(filter.name) < 0)
+            }, true);
+        };
+
     });
 }());
