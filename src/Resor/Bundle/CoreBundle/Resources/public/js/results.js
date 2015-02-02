6c3e54bdd4f951dbcb87e412ac1a05dfab36e0b0
Arthur Himmel <himmel.arthur@gmail.com>
2015-02-02 15:34:04 +0100

    #3 - Cleaning

--- src/Resor/Bundle/CoreBundle/Resources/public/js/results.js
+++ src/Resor/Bundle/CoreBundle/Resources/public/js/results.js
@@ -26,7 +26,7 @@
         };
     });
 
-    var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in dignissim ex. Ut efficitur libero sed ipsum laoreet, a laoreet nunc consequat. Vestibulum mollis quis dui non commodo. Donec nec neque id nulla volutpat maximus. Nulla facilisi. Nullam ultricies lacinia diam, nec consectetur massa congue ac. Phasellus scelerisque at enim sed rhoncus. Ut ut ex leo. Nunc eu libero leo. Phasellus placerat luctus interdum. Duis efficitur laoreet dolor, malesuada rutrum neque congue sit amet. Aliquam semper arcu sapien. Suspendisse diam neque, ullamcorper sed pulvinar nec, tincidunt non mauris. Nulla at sagittis risus. Nullam aliquam, lectus in fermentum blandit, eros risus pellentesque purus, et interdum dolor magna eu arcu.";
+    var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in dignissim ex. Ut efficitur libero sed ipsum laoreet, a laoreet nunc consequat. Vestibulum mollis quis dui non commodo. Donec nec neque id nulla volutpat maximus. Nulla facilisi. Nullam ultricies lacinia diam, nec consectetur massa congue ac. Phasellus scelerisque at enim sed rhoncus. Ut ut ex leo. Nunc eu libero leo. Phasellus placerat luctus interdum. Duis efficitur laoreet dolor, malesuada rutrum neque congue sit amet. Aliquam semper arcu sapien. ";
 
     app.controller('ResultCtrl', function ($scope) {
         $scope.results = [
