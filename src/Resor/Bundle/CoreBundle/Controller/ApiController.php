27e4e2992b77274dd8a69f1258478c814686b391
LudoZeGeek <ludo.zegeek@gmail.com>
2015-03-28 17:13:06 +0100

    #10919 Get bookings api

--- src/Resor/Bundle/CoreBundle/Controller/ApiController.php
+++ src/Resor/Bundle/CoreBundle/Controller/ApiController.php
@@ -3,9 +3,29 @@
 namespace Resor\Bundle\CoreBundle\Controller;
 
 
+use Resor\Bundle\CoreBundle\Entity\Camping;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations\View;
 
 class ApiController extends Controller {
+    /**
+     * @param int $campingId
+     * @return array
+     * @View()
+     */
+    public function getBookingsAction($campingId)
+    {
+        $em = $this->getDoctrine()->getManager();
 
+        $camping = $em->getRepository('ResorCoreBundle:Camping')
+            ->find($campingId);
+
+        $places = $em->getRepository('ResorCoreBundle:Place')
+            ->findByCamping($camping);
+
+        $bookings = $em->getRepository('ResorCoreBundle:Booking')
+            ->findByPlace($places);
+
+        return ['bookings' => $bookings];
+    }
 }
