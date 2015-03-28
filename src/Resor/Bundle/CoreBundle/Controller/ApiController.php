9c33c15dfb8d8fdc776faf4be4d3ef0fdc6869b9
LudoZeGeek <ludo.zegeek@gmail.com>
2015-03-28 16:30:39 +0100

    #10919 Create api controller

---
+++ src/Resor/Bundle/CoreBundle/Controller/ApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Resor\Bundle\CoreBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations\View;
+
+class ApiController extends Controller {
+
+}
