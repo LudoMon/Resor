690b55586a96d9c133d263b8373ab0cc8affdc0f
LudoZeGeek <ludo.zegeek@gmail.com>
2015-03-28 12:23:30 +0100

    #10919 Add bundles to project

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new Resor\Bundle\CoreBundle\ResorCoreBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new \FOS\RestBundle\FOSRestBundle(),
+            new \JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
