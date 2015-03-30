69c9ea3cf04d27dac3e29f1a48125600aa276ef7
Arthur Himmel <himmel.arthur@gmail.com>
2015-03-30 15:27:31 +0200

    #17 - Using DB for booking

--- src/Resor/Bundle/CoreBundle/Controller/BookController.php
+++ src/Resor/Bundle/CoreBundle/Controller/BookController.php
@@ -15,49 +15,10 @@ class BookController extends Controller
      */
     public function bookCampingAction($id, Request $request)
     {
-        $campings = array(
-            array(
-                "id" => 0,
-                "title" => "Camping des flots bleus",
-                "price" => "34",
-                "features" => array("pool", "animals", "spa", "jacuzzi"),
-                "lat" => 48.85934,
-                "lng" => 2.31617
-            ),
-            array(
-                "id" => 1,
-                "title" => "Camping de l'océan",
-                "price" => "27",
-                "features" => array("spa", "jacuzzi"),
-                "lat" => 48.84534,
-                "lng" => 2.33368
-            ),
-            array(
-                "id" => 2,
-                "title" => "Camping de la plage",
-                "price" => "39",
-                "features" => array("pool", "spa", "jacuzzi"),
-                "lat" => 48.83811,
-                "lng" => 2.37522
-            ),
-            array(
-                "id" => 3,
-                "title" => "Camping du soleil",
-                "price" => "42",
-                "features" => array("pool", "animals"),
-                "lat" => 48.86431,
-                "lng" => 2.37248
-            ),
-            array(
-                "id" => 4,
-                "title" => "Camping Serge",
-                "price" => "19",
-                "features" => array("spa", "jacuzzi"),
-                "lat" => 48.89050,
-                "lng" => 2.33471
-            )
-        );
-        $camping = $campings[$id];
+        $camping = $this->getDoctrine()
+            ->getRepository('ResorCoreBundle:Camping')
+            ->find($id);
+
         $from = $request->query->get('from');
         $to = $request->query->get('to');
         return array(
