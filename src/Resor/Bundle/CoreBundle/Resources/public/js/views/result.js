106d944d7330387647ccf7d39998fab8c66c2694
Arthur Himmel <himmel.arthur@gmail.com>
2015-03-30 13:46:57 +0200

    27 - Displaying pictures

--- src/Resor/Bundle/CoreBundle/Resources/public/js/views/result.js
+++ src/Resor/Bundle/CoreBundle/Resources/public/js/views/result.js
@@ -2,6 +2,12 @@ var Marionette = require('backbone.marionette');
 
 module.exports = Marionette.ItemView.extend({
 
-    template: require('../templates/result.jade')
+    template: require('../templates/result.jade'),
+
+    serializeData: function () {
+        return _.extend(this.model.toJSON(), {
+            pictureUrl: '/uploads/pictures/' + this.model.get('picture_path')
+        });
+    }
 
 });
