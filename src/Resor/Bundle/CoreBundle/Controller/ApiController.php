a1cfb37afea2ca3c18667f09530d8232ce9e9c63
LudoZeGeek <ludo.zegeek@gmail.com>
2015-03-30 10:42:36 +0200

    #25 Create post offers api for campings

--- src/Resor/Bundle/CoreBundle/Controller/ApiController.php
+++ src/Resor/Bundle/CoreBundle/Controller/ApiController.php
@@ -3,9 +3,14 @@
 namespace Resor\Bundle\CoreBundle\Controller;
 
 
+use Resor\Bundle\CoreBundle\Entity\Availability;
+use Resor\Bundle\CoreBundle\Entity\Booking;
 use Resor\Bundle\CoreBundle\Entity\Camping;
+use Resor\Bundle\CoreBundle\Entity\Offer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations\View;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class ApiController extends Controller {
     /**
@@ -27,5 +32,83 @@ class ApiController extends Controller {
             ->findByOffer($offers);
 
         return ['bookings' => $bookings];
+    }
+
+    public function postOffersAction($campingId, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $messages = [];
+
+        $camping = $em->getRepository('ResorCoreBundle:Camping')
+            ->find($campingId);
+        $currentOffers = $em->getRepository('ResorCoreBundle:Offer')
+            ->findByCamping($camping);
+        $currentOffersIds = $this->getOffersIds($currentOffers);
+
+        $newOffers = json_decode($request->get('offers'));
+        foreach ($newOffers as $newOffer) {
+            $keyInIds = array_search($newOffer['id'], $currentOffersIds);
+            if (false !== $keyInIds) {
+                // TODO Update offer, comparing availabilities
+                $messages[] = 'Offer ' . $newOffer['id'] . ' already exists';
+                unset($currentOffersIds[$keyInIds]);
+            } else {
+                $offer = new Offer();
+                $offer->setCampingId($newOffer['id'])
+                    ->setCreatedAt($newOffer['created_at'])
+                    ->setUpdatedAt($newOffer['updated_at'])
+                    ->setName($newOffer['name'])
+                    ->setOptions($newOffer['options'])
+                    ->setImages($newOffer['images'])
+                    ->setCamping($camping);
+                $em->persist($offer);
+
+                foreach ($newOffer['plage'] as $plage) {
+                    $availability = new Availability();
+                    $availability->setStartDate($plage['debut'])
+                        ->setEndDate($plage['fin'])
+                        ->setIsOpen($plage['ouvert'])
+                        ->setPrice(isset($plage['prix']) ? $plage['prix'] : 0)
+                        ->setPlacesNumber($plage['nbEmplacements'])
+                        ->setOffer($offer);
+                    $em->persist($availability);
+                }
+            }
+        }
+
+        $bookingRepository = $em->getRepository('ResorCoreBundle:Booking');
+        $today = new DateTime();
+        foreach ($currentOffersIds as $key => $offerToDeleteId) {
+            $bookings = $bookingRepository->findByOffer($currentOffers[$key]);
+            $canDeleteOffer = true;
+            foreach ($bookings as $booking) {
+                if ($booking->getEndDate() >= $today) {
+                    $canDeleteOffer = false;
+                    break;
+                }
+            }
+
+            if ($canDeleteOffer) {
+                $em->remove($currentOffers[$key]);
+            } else {
+                $messages[] = 'Could not delete offer ' . $offerToDeleteId;
+            }
+        }
+
+        $em->flush();
+
+        if (empty($messages)) {
+            $messages[] = 'Data successfuly updated';
+        }
+        return ['messages', $messages];
+    }
+
+    private function getOffersIds($currentOffers)
+    {
+        $ids = [];
+        foreach ($currentOffers as $key => $offer) {
+            $ids[$key] = $offer->getCampingId();
+        }
+        return $ids;
     }
 }
