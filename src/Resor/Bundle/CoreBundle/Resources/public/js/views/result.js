99ec5e6e27c1c8a8f5e359e9d127ddfcc176d86d
Arthur Himmel <himmel.arthur@gmail.com>
2015-04-01 11:51:07 +0200

    blbl

--- src/Resor/Bundle/CoreBundle/Resources/public/js/views/result.js
+++ src/Resor/Bundle/CoreBundle/Resources/public/js/views/result.js
@@ -12,6 +12,7 @@ module.exports = Marionette.ItemView.extend({
     serializeData: function () {
         var dateLimits = this.searchParams.getDateLimits();
         return _.extend(this.model.toJSON(), {
+            hasPicture: this.model.get('picture_path'),
             link: '/book/' + this.model.get('id') + '?from=' + this.searchParams.get('from') + '&to=' + this.searchParams.get('to'),
             pictureUrl: '/uploads/pictures/' + this.model.get('picture_path'),
             minPrice: this.model.getMinPrice(dateLimits.from, dateLimits.to) * this.searchParams.getDuration()
